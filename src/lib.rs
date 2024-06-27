3413c73120955fe3a35b6b09dcdd12b346a2fde2
Rahul Garg <rg@vihu.dev>
2024-06-27 11:36:59 -0400

    Make it a lib

---
+++ src/lib.rs
@@ -0,0 +1,3 @@
+mod data_generator;
+pub mod knee_locator;
+mod shape_detector;
--- src/main.rs
+++
@@ -1,7 +0,0 @@
-mod data_generator;
-mod knee_locator;
-mod shape_detector;
-
-fn main() {
-    println!("Hello, world!");
-}
